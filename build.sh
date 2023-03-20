8b46d4782bdae99621be55b4741d70c739d8bc84
Hisashi Itoh <it.hisas@gmail.com>
2023-03-20 14:22:15 +0000

    ipdate

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-# sudo docker build --no-cache -t gpt-neox .
-sudo docker build -t gpt-neox .
+sudo docker build --no-cache -t gpt-neox .
+#sudo docker build -t gpt-neox .
--- launch.sh
+++ launch.sh
@@ -3,11 +3,12 @@ IMAGE="gpt-neox"
 CONTAINER="gpt-neox"
 sudo docker run -tid \
      --privileged \
+     --gpus all \
      -v /data:/data \
+     --shm-size=64gb \
      --network=host \
      --name ${CONTAINER} \
      ${IMAGE} /bin/bash
 
 # -p 8888:8888 \
 # --shm-size=64gb \
-# --network=host \
